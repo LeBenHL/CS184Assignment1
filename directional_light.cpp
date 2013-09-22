426dc117800e3bb978e315b0e61b10dd6346a688
Lichen <thisislichen@berkeley.edu>
2013-09-22 15:36:06 -0700

    commented out all print statements

--- directional_light.cpp
+++ directional_light.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 DirectionalLight::DirectionalLight(float _x, float _y, float _z, float r, float g, float b) {
 	direction = new ThreeDVector(_x, _y, _z);
-	cout << "BEFORE: " << direction->x << ", " << direction->y << ", " << direction->z << endl;
+	//cout << "BEFORE: " << direction->x << ", " << direction->y << ", " << direction->z << endl;
 	direction->normalize();
-	cout << "AFTER: " << direction->x << ", " << direction->y << ", " << direction->z << endl;
+	//cout << "AFTER: " << direction->x << ", " << direction->y << ", " << direction->z << endl;
 	direction->scalar_multiply(-1);
 	red = r;
 	green = g;
